26d2068a70a4ac2513a778cf11c60c2700c1d13e
ACER <ACER@DESKTOP-PH1R48V>
2025-09-12 10:17:44 -0500

    Add employee entity

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Share.Entities;
+
+namespace Orders.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Employee>()
+            .Property(e => e.Salary)
+            .HasPrecision(18, 2); // ← precisión y escala
+    }
+}
---
+++ Orders/Orders.Share/Entities/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Share.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(30, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string FirstName { get; set; } = null;
+
+        [Display(Name = "Apellido")]
+        [MaxLength(30, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string LastName { get; set; } = null;
+
+        [Display(Name = "Activo")]
+        public bool IsActive { get; set; } = true;
+
+        [Display(Name = "Fecha de contratación")]
+        [DataType(DataType.Date)]
+        public DateTime HireDate { get; set; } = DateTime.UtcNow;
+
+        [Display(Name = "Salario")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Range(1000000, double.MaxValue, ErrorMessage = "El campo {0} debe ser mínimo {1:C0}")]
+        public decimal Salary { get; set; }
+    }
+}
