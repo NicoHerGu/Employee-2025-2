982711ccb5a66b9f13c0ebfe8f5ee26f152f347a
ACER <ACER@DESKTOP-PH1R48V>
2025-09-12 11:49:51 -0500

    method Put and Get

---
+++ Orders/Orders.Backend/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Share.Entities;
+using System.Threading.Tasks;
+
+namespace Orders.Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public EmployeesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        _context.SaveChanges();
+        await _context.SaveChangesAsync();
+        return Ok(employee);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Employees.ToListAsync());
+    }
+}
