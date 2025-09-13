80d405c4cafe20a7bff80590b5cbe3f216e56dbe
ACER <ACER@DESKTOP-PH1R48V>
2025-09-13 01:47:46 -0500

    Creation of method search

--- Orders/Orders.Backend/Controllers/EmployeesController.cs
+++ Orders/Orders.Backend/Controllers/EmployeesController.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
 using Orders.Backend.UnitsOfWork.Interface;
 using Orders.Share.Entities;
-using System.Threading.Tasks;
 
 namespace Orders.Backend.Controllers;
 
@@ -11,7 +10,26 @@ namespace Orders.Backend.Controllers;
 [Route("api/[controller]")]
 public class EmployeesController : GenericController<Employee>
 {
-    public EmployeesController(IGenericUnitOfWork<Employee> unitOfWork) : base(unitOfWork)
+    private readonly DataContext _context;
+
+    public EmployeesController(IGenericUnitOfWork<Employee> unitOfWork, DataContext context)
+        : base(unitOfWork)
     {
+        _context = context;
+    }
+
+    [HttpGet("search")]
+    public async Task<ActionResult<IEnumerable<Employee>>> SearchEmployees([FromQuery] string query)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+        {
+            return BadRequest("Debe enviar un parámetro de búsqueda.");
+        }
+
+        var employees = await _context.Employees
+            .Where(e => e.FirstName.Contains(query) || e.LastName.Contains(query))
+            .ToListAsync();
+
+        return Ok(employees);
     }
 }
